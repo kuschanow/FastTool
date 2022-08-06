330abd22fd57ff604ac9b11c8881e51c6737984a
RomanKuschanow <kuschanow@gmail.com>
2022-08-06 18:27:34 +0300

    ViewModel

--- FastTool.Wpf/MainWindow.xaml.cs
+++ FastTool.Wpf/MainWindow.xaml.cs
@@ -1,28 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FastTool.Models;
+using Microsoft.EntityFrameworkCore;
+using FastTool.HotKey;
+using System.ComponentModel;
 
-namespace FastTool
+namespace FastTool.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool TrayClose = false;
+        MainWindowViewModel mainWindowViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            mainWindowViewModel = new MainWindowViewModel();
+            mainWindowViewModel.PropertyChanged += MainWindowViewModel_PropertyChanged;
+            DataContext = mainWindowViewModel;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using var db = new DBContext();
+            db.Database.Migrate();
+        }
+
+        private void MainWindowViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "MainWindowVisibility")
+            {
+                Visibility = mainWindowViewModel.MainWindowVisibility;
+            }
+        }
+
+        private void Form_Closing(object sender, CancelEventArgs e)
+        {
+            if (!TrayClose)
+            {
+                e.Cancel = true;
+                mainWindowViewModel.ChangeWindowVisibility.Execute(mainWindow);
+            }
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, RoutedEventArgs e)
+        {
+            mainWindowViewModel.ChangeWindowVisibility.Execute(null);
+            WindowState = WindowState.Normal;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            TrayClose = true;
+            this.Close();
         }
     }
 }
---
+++ FastTool.Wpf/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FastTool.WPF
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private Visibility _mainWindowVisibility = Visibility.Visible;
+        private Visibility _notifyIconVisibility = Visibility.Hidden;
+
+
+        public Visibility MainWindowVisibility 
+        { 
+            get => _mainWindowVisibility; 
+            set 
+            { 
+                _mainWindowVisibility = value;
+                OnPropertyChanged();
+                NotifyIconVisibility = MainWindowVisibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
+            }
+        }
+        public Visibility NotifyIconVisibility
+        { 
+            get => _notifyIconVisibility; 
+            set 
+            { 
+                _notifyIconVisibility = value;
+                OnPropertyChanged(); 
+            } 
+        }
+
+
+        public ICommand ChangeWindowVisibility => new RelayCommand(ChangeWindowVisibilityExecute);
+
+
+        private void ChangeWindowVisibilityExecute(object obj)
+        {
+            MainWindowVisibility = MainWindowVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Predicate<object>? _canExecute;
+        private readonly Action<object> _execute;
+        public event EventHandler? CanExecuteChanged;
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+        public RelayCommand(Action<object> execute, Predicate<object>? canExecute) { _execute = execute; _canExecute = canExecute; }
+        public bool CanExecute(object parameter) => (_canExecute == null) ? true : _canExecute(parameter);
+        public void Execute(object parameter) => _execute(parameter);
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
