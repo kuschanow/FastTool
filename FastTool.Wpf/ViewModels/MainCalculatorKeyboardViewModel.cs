4c49d6be4808af020f319a8dd48fcdfa54633e53
RomanKuschanow <kuschanow@gmail.com>
2022-08-12 12:28:54 +0300

    Additional buttons

--- FastTool.Wpf/ViewModels/MainCalculatorKeyboardViewModel.cs
+++ FastTool.Wpf/ViewModels/MainCalculatorKeyboardViewModel.cs
@@ -3,21 +3,59 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows;
 using System.Windows.Input;
 
 namespace FastTool.WPF
 {
     public class MainCalculatorKeyboardViewModel : INotifyPropertyChanged
     {
+        private Visibility powOpt = Visibility.Collapsed;
+        private Visibility rootOpt = Visibility.Collapsed;
+
         #region prors
+        public Visibility PowOpt
+        {
+            get => powOpt;
+            set
+            {
+                powOpt = value;
+                OnPropertyChanged();
+            }
+        }
+        public Visibility RootOpt
+        {
+            get => rootOpt;
+            set
+            {
+                rootOpt = value;
+                OnPropertyChanged();
+            }
+        }
         #endregion
 
 
         public ICommand Write => new RelayCommand(WriteExecute);
+        public ICommand ShowOpt => new RelayCommand(ShoeOptExecute);
 
         private void WriteExecute(object obj)
         {
             OnKeyPressed((string)obj);
+            PowOpt = Visibility.Collapsed;
+            RootOpt = Visibility.Collapsed;
+        }
+        private void ShoeOptExecute(object obj)
+        {
+            if ((string)obj == "pow")
+            {
+                PowOpt = Visibility.Visible;
+                RootOpt = Visibility.Collapsed;
+            }
+            else if ((string)obj == "root")
+            {
+                RootOpt = Visibility.Visible;
+                PowOpt = Visibility.Collapsed;
+            }
         }
 
         #region KeyPressed
--- FastTool.Wpf/ViewModels/MainCalculatorViewModel.cs
+++ FastTool.Wpf/ViewModels/MainCalculatorViewModel.cs
@@ -96,6 +96,7 @@ namespace FastTool.WPF
                 OnPropertyChanged();
             }
         }
+        
         #endregion
 
 
