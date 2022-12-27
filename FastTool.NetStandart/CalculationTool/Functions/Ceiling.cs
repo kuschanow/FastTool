039cff58579e8f6ba3830c55a0d3c901ffca040b
RomanKuschanow <kuschanow@gmail.com>
2022-12-27 21:29:53 +0200

    floor and ceiling functions

---
+++ FastTool.NetStandart/CalculationTool/Functions/Ceiling.cs
@@ -0,0 +1,26 @@
+﻿using FastTool.CalculationTool.Interfaces;
+using System;
+using System.Numerics;
+
+namespace FastTool.CalculationTool.Functions
+{
+    public class Ceiling : IFunction
+    {
+        public string[] Names => new string[] { "ceiling" };
+
+        public ICalculateble[] Args { get; }
+
+        public Ceiling(ICalculateble[] args) => Args = args;
+
+        public Complex Calculate(Mode mode)
+        {
+            Complex complex = Args[0].Calculate(mode);
+            double real = Math.Ceiling(complex.Real);
+            double imaginary = Math.Ceiling(complex.Imaginary);
+
+            return new(real, imaginary);
+        }
+
+        public override string ToString() => $"ceiling({Args[0]})";
+    }
+}
---
+++ FastTool.NetStandart/CalculationTool/Functions/Floor.cs
@@ -0,0 +1,26 @@
+﻿using FastTool.CalculationTool.Interfaces;
+using System;
+using System.Numerics;
+
+namespace FastTool.CalculationTool.Functions
+{
+    public class Floor : IFunction
+    {
+        public string[] Names => new string[] { "floor" };
+
+        public ICalculateble[] Args { get; }
+
+        public Floor(ICalculateble[] args) => Args = args;
+
+        public Complex Calculate(Mode mode)
+        {
+            Complex complex = Args[0].Calculate(mode);
+            double real = Math.Floor(complex.Real);
+            double imaginary = Math.Floor(complex.Imaginary);
+
+            return new(real, imaginary);
+        }
+
+        public override string ToString() => $"floor({Args[0]})";
+    }
+}
