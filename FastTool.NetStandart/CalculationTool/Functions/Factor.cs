01b583cd7b227594fcd0be4587d1070f8d72f8fd
RomanKuschanow <kuschanow@gmail.com>
2022-12-28 12:10:38 +0200

    Gamma function approximation

--- FastTool.NetStandart/CalculationTool/Functions/Factor.cs
+++ FastTool.NetStandart/CalculationTool/Functions/Factor.cs
@@ -1,11 +1,12 @@
 ﻿using FastTool.CalculationTool.Interfaces;
+using System;
 using System.Numerics;
 
 namespace FastTool.CalculationTool.Functions;
 
 public class Factor : IFunction
 {
-    public string[] Names => new string[] { "factor", "factorial" };
+    public string[] Names => new string[] { "fact", "factor", "factorial" };
 
     public ICalculateble[] Args { get; }
 
@@ -13,22 +14,48 @@ public class Factor : IFunction
 
     public Complex Calculate(Mode mode)
     {
-        uint num = (uint)Args[0].Calculate(mode).Real;
+        Complex num = Args[0].Calculate(mode) + 1;
 
-        return new Complex(Factorial(num), 0);
+        return Gamma(num);
     }
 
-    public double Factorial(uint num)
+    private Complex GammaApprox(Complex x)
     {
-        int result = 1;
+        double[] p = { -1.71618513886549492533811e+0, 2.47656508055759199108314e+1, -3.79804256470945635097577e+2, 6.29331155312818442661052e+2, 8.66966202790413211295064e+2, -3.14512729688483675254357e+4, -3.61444134186911729807069e+4, 6.64561438202405440627855e+4 };
 
-        for (int i = 1; i < num; i++)
+        double[] q = { -3.08402300119738975254353e+1, 3.15350626979604161529144e+2, -1.01515636749021914166146e+3, -3.10777167157231109440444e+3, 2.25381184209801510330112e+4, 4.75584627752788110767815e+3, -1.34659959864969306392456e+5, -1.15132259675553483497211e+5 };
+        Complex z = x - 1.0;
+        Complex a = 0.0;
+        Complex b = 1.0;
+        int i;
+        for (i = 0; i < 8; i++)
         {
-            result *= i + 1;
+            a = (a + p[i]) * z;
+            b = b * z + q[i];
+        }
+        return (a / b + 1.0);
+    }
+
+    private Complex Gamma(Complex z)
+    {
+
+        if ((z.Real > 0) && (z.Real < 1.0))
+        {
+            return Gamma(z + 1.0) / z;
         }
 
-        return result;
+        if (z.Real > 2)
+        {
+            return (z - 1) * Gamma(z - 1);
+        }
+
+        if (z.Real <= 0)
+        {
+            return Math.PI / (Complex.Sin(Math.PI * z) * Gamma(1 - z));
+        }
+
+        return GammaApprox(z);
     }
 
-    public override string ToString() => $"{Args[0]}!";
+    public override string ToString() => $"fact({Args[0]})";
 }
---
+++ FastTool.Tests/FunctionsTests.cs
@@ -0,0 +1,21 @@
+﻿using FastTool.CalculationTool.Functions;
+using FastTool.CalculationTool.Interfaces;
+
+namespace FastTool.Tests
+{
+    public class FunctionsTests
+    {
+        [Fact]
+        public void FactorialTest()
+        {
+            // Arrange
+            var sut = new Factor(new ICalculateble[] { new GetComplex(new ICalculateble[] {new Number(-4), new Number(-0.5)}) });
+
+            //Act
+            var answer = sut.Calculate(Mode.Deg);
+
+            //Assert
+            answer.Magnitude.Should().Be(0.23571275348885148);
+        }
+    }
+}
