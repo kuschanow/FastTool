4b64dd65e5b582da41b74091bd08e356b948be4a
RomanKuschanow <kuschanow@gmail.com>
2023-01-03 23:39:22 +0200

    Button refactoring

--- FastTool.WPF/Controls/Calculator/MemoryItem.xaml.cs
+++ FastTool.WPF/Controls/Calculator/MemoryItem.xaml.cs
@@ -52,7 +52,7 @@ namespace FastTool.WPF.Controls
             set => SetValue(DeleteCommandProperty, value);
         }
 
-        public static readonly DependencyProperty DeletePropertyProperty = DependencyProperty.Register("DeleteProperty", typeof(object), typeof(MemoryItem));
+        public static readonly DependencyProperty DeleteCommandProperty = DependencyProperty.Register("DeleteCommand", typeof(ICommand), typeof(MemoryItem));
 
         public object DeleteProperty
         {
@@ -60,7 +60,7 @@ namespace FastTool.WPF.Controls
             set => SetValue(DeletePropertyProperty, value);
         }
 
-        public static readonly DependencyProperty DeleteCommandProperty = DependencyProperty.Register("DeleteCommand", typeof(ICommand), typeof(MemoryItem));
+        public static readonly DependencyProperty DeletePropertyProperty = DependencyProperty.Register("DeleteProperty", typeof(object), typeof(MemoryItem));
 
         public string Expression
         {
